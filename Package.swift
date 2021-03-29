90d90300bc66f57130326ce555d474fcd56856c9
Andrew Breckenridge <asbreckenridge@me.com>
2021-03-28 18:47:45 -0700

    Use Iron-Ham/SnapKit-Dynamic

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.3
 import PackageDescription
 
 let package = Package(
@@ -10,13 +10,13 @@ let package = Package(
         .library(name: "NotificationBannerSwift", targets: ["NotificationBannerSwift"])
     ],
     dependencies: [
-        .package(url: "https://github.com/SnapKit/SnapKit", from: "5.0.1"),
+        .package(url: "https://github.com/Iron-Ham/SnapKit", .revision("7e869d8781c9b2f0c2b00c811aaf6ec8736aff2a")),
         .package(url: "https://github.com/cbpowell/MarqueeLabel", from: "4.0.1")
     ],
     targets: [
         .target(
             name: "NotificationBannerSwift",
-            dependencies: [.byName(name: "MarqueeLabel"), .byName(name: "SnapKit")],
+            dependencies: [.byName(name: "MarqueeLabel"), .product(name: "SnapKit-Dynamic", package: "SnapKit")],
             path: "NotificationBanner/Classes"
         )
     ],
